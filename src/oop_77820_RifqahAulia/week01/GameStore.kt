08249052c80948ac0a2ad6f8fb8236652decc1bb
Rifqah Aulia <rifqahauliasalsabilla173@gmail.com>
2026-02-17 16:49:19 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_77820_RifqahAulia/week01/GameStore.kt
+++ src/oop_77820_RifqahAulia/week01/GameStore.kt
@@ -1,7 +1,14 @@
 package oop_77820_RifqahAulia.week01
 
+fun calculateDiscount(price: Int): Int =
+    if (price > 500000) price * 20 / 100
+    else price * 10 / 100
+
 fun main() {
 
     val gameTitle = "Cyber Adventure"
     val price = 750000
+
+    val discount = calculateDiscount(price)
+    val finalPrice = price - discount
 }
