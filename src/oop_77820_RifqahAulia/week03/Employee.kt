28a46e6b1e01a7f3dfeefe19f364fbd91d1b9bcd
Rifqah Aulia <rifqahauliasalsabilla173@gmail.com>
2026-02-20 13:36:08 +0700

    "week03: cleanup code"

--- src/oop_77820_RifqahAulia/week03/Employee.kt
+++ src/oop_77820_RifqahAulia/week03/Employee.kt
@@ -2,31 +2,24 @@ package oop_77820_RifqahAulia.week03
 
 class Employee(val name: String) {
 
-    // Salary dengan validasi
     var salary: Int = 0
         set(value) {
-            if (value < 0) {
-                println("ERROR: Gaji tidak boleh negatif! Di-set ke 0.")
-                field = 0
-            } else {
-                field = value
-            }
+            field = if (value < 0) 0 else value
         }
 
-    // Private property (Encapsulation)
     private var performanceRating: Int = 3
 
     fun increasePerformance() {
         performanceRating++
-        println("Kinerja $name meningkat! Rating: $performanceRating")
     }
 
     fun printStatus() {
-        println("Karyawan: $name, Rating: $performanceRating")
+        println("Karyawan: $name")
+        println("Gaji: $salary")
+        println("Rating: $performanceRating")
+        println("Pajak (10%): $tax")
     }
 
-    // Computed Property (Custom Getter)
-    // Pajak 10% dari salary
     val tax: Double
         get() = salary * 0.1
 }
