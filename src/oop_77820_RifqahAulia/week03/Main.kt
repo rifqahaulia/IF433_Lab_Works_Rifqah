d6acd9eefb2c57843832d8c10d7aca1306f79ae5
Rifqah Aulia <rifqahauliasalsabilla173@gmail.com>
2026-02-20 13:40:27 +0700

    week03: (task 2) implement Player testing in main

--- src/oop_77820_RifqahAulia/week03/Main.kt
+++ src/oop_77820_RifqahAulia/week03/Main.kt
@@ -2,13 +2,8 @@ package oop_77820_RifqahAulia.week03
 
 fun main() {
 
-    val weapon = Weapon("Excalibur")
+    val player = Player("Rifqah")
 
-    weapon.damage = -50     // Harus gagal
-    println("Damage sekarang: ${weapon.damage}")
-
-    weapon.damage = 9999    // Harus jadi 1000
-    println("Damage sekarang: ${weapon.damage}")
-
-    println("Tier weapon: ${weapon.tier}")
+    player.addXp(50)
+    player.addXp(60)
 }
