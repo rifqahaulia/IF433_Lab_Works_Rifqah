d53c46524ab13e896bc2e3846bbb3ea15f6a13b4
Rifqah Aulia <rifqahauliasalsabilla173@gmail.com>
2026-02-28 21:18:09 +0700

    "week04: (task 2) test Employee hierarchy in main"

--- src/oop_77820_RifqahAulia/week04/Main.kt
+++ src/oop_77820_RifqahAulia/week04/Main.kt
@@ -1,5 +1,6 @@
 package oop_77820_RifqahAulia.week04
 
+
 fun main() {
     println("--- Testing Vehicle ---")
     val generalVehicle = Vehicle(brand = "Sepeda Onthel")
@@ -17,4 +18,13 @@ fun main() {
     myElectricCar.accelerate()
     myElectricCar.honk()
     myElectricCar.openTrunk()
+
+    println("\n--- Testing Employee Hierarchy ---")
+    val manager = Manager(name = "Budi", baseSalary = 10_000_000)
+    manager.work()
+    println("Bonus ${manager.name}: ${manager.calculateBonus()}")
+
+    val developer = Developer(name = "Ani", baseSalary = 8_000_000, programmingLanguage = "Kotlin")
+    developer.work()
+    println("Bonus ${developer.name}: ${developer.calculateBonus()}")
 }
