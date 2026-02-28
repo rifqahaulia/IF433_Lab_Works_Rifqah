e996fccd3894c1963c8d97db53029bd1621d0cb4
Rifqah Aulia <rifqahauliasalsabilla173@gmail.com>
2026-02-28 20:49:17 +0700

    "week04: create Car class extending Vehicle"

---
+++ src/oop_77820_RifqahAulia/week04/Car.kt
@@ -0,0 +1,7 @@
+package oop_77820_RifqahAulia.week04
+open class Car(brand: String, val numberOfDoors: Int) : Vehicle(brand) {
+
+    fun openTrunk() {
+        println("Bagasi mobil $brand dengan $numberOfDoors pintu dibuka.")
+    }
+}
