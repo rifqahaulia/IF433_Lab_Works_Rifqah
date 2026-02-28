6bbe6003631c91fff873e02629832850746bdec5
Rifqah Aulia <rifqahauliasalsabilla173@gmail.com>
2026-02-28 20:56:35 +0700

    "week04: (task 2) create parent class Employee"

---
+++ src/oop_77820_RifqahAulia/week04/Employee.kt
@@ -0,0 +1,12 @@
+package oop_77820_RifqahAulia.week04
+
+open class Employee(val name: String, val baseSalary: Int) {
+
+    open fun work() {
+        println("$name sedang bekerja.")
+    }
+
+    open fun calculateBonus(): Int {
+        return baseSalary / 10 // 10% dari baseSalary
+    }
+}
