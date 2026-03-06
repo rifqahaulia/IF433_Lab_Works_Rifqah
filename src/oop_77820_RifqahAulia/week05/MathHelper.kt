b3af40b9e0f1fb1d82bca5c698c2b4c67652fc6c
Rifqah Aulia <rifqahauliasalsabilla173@gmail.com>
2026-03-06 15:02:08 +0700

    week05: (task 1) create MathHelper class with overloading

---
+++ src/oop_77820_RifqahAulia/week05/MathHelper.kt
@@ -0,0 +1,16 @@
+package oop_77820_RifqahAulia.week05
+
+class MathHelper {
+
+    fun hitungLuas(sisi: Int): Int {
+        return sisi * sisi
+    }
+
+    fun hitungLuas(panjang: Int, lebar: Int): Int {
+        return panjang * lebar
+    }
+
+    fun hitungLuas(jariJari: Double): Double {
+        return 3.14 * jariJari * jariJari
+    }
+}
