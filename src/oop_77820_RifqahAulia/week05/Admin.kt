1ba64e88865d37d38b26e3a232894594848a8bc9
Rifqah Aulia <rifqahauliasalsabilla173@gmail.com>
2026-03-06 15:00:06 +0700

    week05: create Admin class inheriting Pegawai (REVISI)

--- src/oop_77820_RifqahAulia/week05/Admin.kt
+++ src/oop_77820_RifqahAulia/week05/Admin.kt
@@ -1,4 +1,12 @@
 package oop_77820_RifqahAulia.week05
 
-class Admin {
+class Admin(nama: String) : Pegawai(nama) {
+
+    override fun bekerja() {
+        println("$nama sedang duduk di depan komputer melayani administrasi.")
+    }
+
+    fun doAdminWork() {
+        println("$nama sedang merekap data absensi mahasiswa.")
+    }
 }
