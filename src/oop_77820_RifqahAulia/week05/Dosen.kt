f680712cf479bf4403688e380f15d23fcb65e98a
Rifqah Aulia <rifqahauliasalsabilla173@gmail.com>
2026-03-06 14:58:05 +0700

    week05: create Dosen class inheriting Pegawai

---
+++ src/oop_77820_RifqahAulia/week05/Dosen.kt
@@ -0,0 +1,12 @@
+package oop_77820_RifqahAulia.week05
+
+class Dosen(nama: String, val nidn: String) : Pegawai(nama) {
+
+    override fun bekerja() {
+        println("$nama sedang menyiapkan materi perkuliahan dan merevisi RPKPS.")
+    }
+
+    fun mengajar() {
+        println("$nama sedang mengajar mahasiswa di kelas.")
+    }
+}
