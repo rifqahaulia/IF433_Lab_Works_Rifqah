d8bf533b0092ecf627ffd3765265c98e84360264
Rifqah Aulia <rifqahauliasalsabilla173@gmail.com>
2026-03-13 15:09:43 +0700

    "week06: (task) implement advanced smart casting in Hub"

--- src/oop_77820_RifqahAulia/week06/SmartHomeHub.kt
+++ src/oop_77820_RifqahAulia/week06/SmartHomeHub.kt
@@ -16,4 +16,16 @@ class SmartHomeHub {
             }
         }
     }
+
+    fun activateSecurityMode() {
+        println("\n=== Mengaktifkan Security Mode ===")
+        for (device in devices) {
+            if (device is Recordable) {
+                device.startRecord()
+            }
+            if (device is SmartSpeaker) {
+                device.playMusic("Sirine Peringatan")
+            }
+        }
+    }
 }
