93f5b595549ebea37e8aca1e3ae88eb21f59f276
Rifqah Aulia <rifqahauliasalsabilla173@gmail.com>
2026-03-13 15:04:12 +0700

    "week06: create Gopay and CreditCard"

---
+++ src/oop_77820_RifqahAulia/week06/PaymentImplementations.kt
@@ -0,0 +1,13 @@
+package oop_77820_RifqahAulia.week06
+
+class Gopay : PaymentMethod {
+    override fun pay(amount: Double) {
+        println("Processing Rp$amount via Gopay Server")
+    }
+}
+
+class CreditCard : PaymentMethod {
+    override fun pay(amount: Double) {
+        println("Contacting Bank for Rp$amount")
+    }
+}
