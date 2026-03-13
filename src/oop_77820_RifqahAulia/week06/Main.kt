941f98457446c7c9872a1112cf49f248d557b098
Rifqah Aulia <rifqahauliasalsabilla173@gmail.com>
2026-03-13 15:11:39 +0700

    "week06: (task) test smart home system logic in main"

--- src/oop_77820_RifqahAulia/week06/Main.kt
+++ src/oop_77820_RifqahAulia/week06/Main.kt
@@ -8,14 +8,20 @@ fun processCheckout(method: PaymentMethod, amount: Double) {
 fun main() {
     val myWatch = Smartwatch()
     myWatch.showTime()
+    myWatch.connectToBluetooth()
+    myWatch.chargeBattery()
+
+    println()
 
     val myPhone = Smartphone()
     myPhone.turnOn()
 
+    println()
+
     val pay1 = Gopay()
     val pay2 = CreditCard()
 
-    println("\n=== TESTING CHECKOUT ===")
+    println("=== TESTING CHECKOUT ===")
     processCheckout(method = pay1, amount = 50000.0)
     processCheckout(method = pay2, amount = 150000.0)
 
@@ -23,4 +29,12 @@ fun main() {
     val lamp = SmartLamp("L001", "Ruang Tamu")
     val speaker = SmartSpeaker("S001", "Google Nest Dapur")
     val cctv = SmartCCTV("C001", "Ezviz Garasi")
+
+    val hub = SmartHomeHub()
+    hub.addDevice(lamp)
+    hub.addDevice(speaker)
+    hub.addDevice(cctv)
+
+    hub.activateSecurityMode()
+    hub.turnOffAllSwitches()
 }
