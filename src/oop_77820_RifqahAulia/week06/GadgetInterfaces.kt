9860ad6c1b032eb94d2ef11baa3ee55ddf18391b
Rifqah Aulia <rifqahauliasalsabilla173@gmail.com>
2026-03-13 14:52:25 +0700

    "week06: create Camera and Phone interfaces with default methods"

---
+++ src/oop_77820_RifqahAulia/week06/GadgetInterfaces.kt
@@ -0,0 +1,13 @@
+package oop_77820_RifqahAulia.week06
+
+interface Camera {
+    fun turnOn() {
+        println("Lensa kamera terbuka dan sensor aktif.")
+    }
+}
+
+interface Phone {
+    fun turnOn() {
+        println("Sinyal seluler mencari jaringan.")
+    }
+}
