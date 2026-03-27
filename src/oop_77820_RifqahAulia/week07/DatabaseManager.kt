53345313125efaaa4bc0ab1405ddbede1c7a2be5
Rifqah Aulia <rifqahauliasalsabilla173@gmail.com>
2026-03-27 19:03:50 +0700

    week07: create singleton DatabaseManager

---
+++ src/oop_77820_RifqahAulia/week07/DatabaseManager.kt
@@ -0,0 +1,10 @@
+package oop_77820_RifqahAulia.week07
+
+object DatabaseManager {
+    var connectionStatus: String = "Disconnected"
+
+    fun connect() {
+        connectionStatus = "Connected to Server"
+        println("Database is ready.")
+    }
+}
