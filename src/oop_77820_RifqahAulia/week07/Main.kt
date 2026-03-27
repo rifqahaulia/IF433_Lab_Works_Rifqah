5d4fda2b01c02bd2fa26f3f133fe880694e10611
Rifqah Aulia <rifqahauliasalsabilla173@gmail.com>
2026-03-27 19:27:56 +0700

    week07: (task) test data class copy and sealed class event dispatch

--- src/oop_77820_RifqahAulia/week07/Main.kt
+++ src/oop_77820_RifqahAulia/week07/Main.kt
@@ -47,4 +47,11 @@ fun main() {
 
     val weapon = Weapon.forgeStarterSword()
     println("Weapon: ${weapon.item}")
+
+    val upgradedItem = weapon.item.copy(damage = 25)
+
+    processEvent(BattleState.SafeZone)
+    processEvent(BattleState.MonsterEncounter("Goblin Nakal"))
+    processEvent(BattleState.LootDropped(upgradedItem))
+    processEvent(BattleState.GameOver("Terkena jebakan racun"))
 }
