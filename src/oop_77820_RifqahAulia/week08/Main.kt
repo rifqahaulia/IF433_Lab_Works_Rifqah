b0fabb59f030f954cc08e1f7f962d64a4d0f3484
Rifqah Aulia <rifqahauliasalsabilla173@gmail.com>
2026-04-17 15:33:32 +0700

    "week08: implement java interop using !!"

--- src/oop_77820_RifqahAulia/week08/Main.kt
+++ src/oop_77820_RifqahAulia/week08/Main.kt
@@ -52,7 +52,6 @@ fun main() {
         println("CRASH (NPE)! Jangan gunakan !! secara sembarangan.")
     }
 
-
     val apiResponse: Map<String, String?> =
         mapOf("status" to "200", "token" to null)
 
@@ -63,4 +62,11 @@ fun main() {
     } catch (e: IllegalArgumentException) {
         println(e.message)
     }
+
+
+    println("\n=== TEST JAVA INTEROP ===")
+    val javaResponse = LegacyJavaAPI.fetchServerStatus()
+    val statusLength = javaResponse!!.length
+
+    println("Status dari Java: $javaResponse (Length: $statusLength)")
 }
