552aaed259dbb59f401e7074f8d382f658c428c7
Rifqah Aulia <rifqahauliasalsabilla173@gmail.com>
2026-04-17 15:36:54 +0700

    "week08: (task) create JavaPaymentService for payment interop"

---
+++ src/oop_77820_RifqahAulia/week08/JavaPaymentService.java
@@ -0,0 +1,8 @@
+package oop_77820_RifqahAulia.week08;
+
+public class JavaPaymentService {
+
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
