e81e6fd71a9c88cff01f710c01572d75a19eb09c
Rifqah Aulia <rifqahauliasalsabilla173@gmail.com>
2026-04-17 15:38:45 +0700

    "week08: (task) process mixed raw JSON list using as?"

---
+++ src/oop_77820_RifqahAulia/week08/ECommerceMain.kt
@@ -0,0 +1,27 @@
+package oop_77820_RifqahAulia.week08
+
+fun main() {
+
+    val rawApiData: List<Map<String, Any?>> = listOf(
+        mapOf("id" to "E01", "name" to "Laptop", "type" to "ELECTRONIC", "warranty" to 24),
+        mapOf("id" to "C01", "name" to "T-Shirt", "type" to "CLOTHING", "size" to "XL"),
+        mapOf("id" to "E02", "name" to "Mouse", "type" to "ELECTRONIC", "warranty" to "Not Int"),
+        mapOf("name" to "Ghost Item", "type" to "CLOTHING"),
+        mapOf("id" to "X01", "name" to "Unknown", "type" to "FOOD")
+    )
+
+    val parser = ApiParser()
+
+    for (raw in rawApiData) {
+        try {
+            val product = parser.parseProduct(raw)
+
+            if (product != null) {
+                println("Parsed product: $product")
+            }
+
+        } catch (e: IllegalArgumentException) {
+            println("ERROR: ${e.message}")
+        }
+    }
+}
