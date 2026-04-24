939d51c85e8ddc438563de5d8c91f7fd02cc9747
Rifqah Aulia <rifqahauliasalsabilla173@gmail.com>
2026-04-24 15:21:05 +0700

    "week09: (task) chain sortedBy and map for worst performers"

--- src/oop_77820_RifqahAulia/week09/TradingDashboard.kt
+++ src/oop_77820_RifqahAulia/week09/TradingDashboard.kt
@@ -20,4 +20,10 @@ fun main() {
         .map {
             "WIN [${it.pair} - ${it.position}]: +${it.roe}% ROE (Lev: ${it.leverage}x)"
         }
+
+    val worstPerformersString = losingTrades
+        .sortedBy { it.roe }
+        .map {
+            "LOSS [${it.pair} - ${it.position}]: ${it.roe}% ROE (Lev: ${it.leverage}x)"
+        }
 }
