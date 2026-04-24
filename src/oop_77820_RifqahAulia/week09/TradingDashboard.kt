505cae3484e8cb6f8aeeefc80b90adba46fe7aab
Rifqah Aulia <rifqahauliasalsabilla173@gmail.com>
2026-04-24 15:21:28 +0700

    "week09: (task) extract unique trading pairs using map and toSet"

--- src/oop_77820_RifqahAulia/week09/TradingDashboard.kt
+++ src/oop_77820_RifqahAulia/week09/TradingDashboard.kt
@@ -26,4 +26,8 @@ fun main() {
         .map {
             "LOSS [${it.pair} - ${it.position}]: ${it.roe}% ROE (Lev: ${it.leverage}x)"
         }
+
+    val uniquePairs = tradeHistory
+        .map { it.pair }
+        .toSet()
 }
