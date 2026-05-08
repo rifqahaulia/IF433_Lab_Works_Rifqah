2bd8ce29b473e0fbf915a4a716b4c0faf993a883
Rifqah Aulia <rifqahauliasalsabilla173@gmail.com>
2026-05-08 15:28:35 +0700

    week10: (task) populate coin repository

--- src/oop_77820_RifqahAulia/week10/CryptoDashboard.kt
+++ src/oop_77820_RifqahAulia/week10/CryptoDashboard.kt
@@ -3,4 +3,8 @@ package oop_77820_RifqahAulia.week10
 fun main() {
 
     val coinRepo = WalletRepository<Coin>()
+
+    coinRepo.add(Coin("BTC", 1.5))
+    coinRepo.add(Coin("ETH", 10.0))
+    coinRepo.add(Coin("USDT", 2500.0))
 }
