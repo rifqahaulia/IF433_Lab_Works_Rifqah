b776436a3c323a1b849fa547fbea3d18dfa956ab
Rifqah Aulia <rifqahauliasalsabilla173@gmail.com>
2026-05-08 15:21:48 +0700

    week10: (task) create Coin and Transaction models

---
+++ src/oop_77820_RifqahAulia/week10/WalletModels.kt
@@ -0,0 +1,11 @@
+package oop_77820_RifqahAulia.week10
+
+data class Coin(
+    val name: String,
+    val balance: Double
+)
+
+data class Transaction(
+    val id: String,
+    val amount: Double
+)
