1c6ecce72817b57a73e246e1186bdcdd31e754ec
Rifqah Aulia <rifqahauliasalsabilla173@gmail.com>
2026-05-08 15:20:03 +0700

    week10: implement where clause for comparable constraints

--- src/oop_77820_RifqahAulia/week10/NumericalStorage.kt
+++ src/oop_77820_RifqahAulia/week10/NumericalStorage.kt
@@ -1,4 +1,18 @@
 package oop_77820_RifqahAulia.week10
 
-class NumericalStorage {
+class MathBox<T : Number>(
+    val value1: T,
+    val value2: T
+) {
+
+    fun sum(): Double {
+
+        return value1.toDouble() + value2.toDouble()
+    }
+}
+
+fun <T> compareValues(a: T, b: T): T
+        where T : Comparable<T> {
+
+    return if (a > b) a else b
 }
