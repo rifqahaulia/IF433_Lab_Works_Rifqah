d33b8e949bd5d670d7d7506bd187697b76e796de
Rifqah Aulia <rifqahauliasalsabilla173@gmail.com>
2026-05-08 14:44:28 +0700

    week11: create string extension functions

---
+++ src/oop_77820_RifqahAulia/week11/StringExtensions.kt
@@ -0,0 +1,11 @@
+package oop_77820_RifqahAulia.week11
+
+// Extension function dengan receiver 'this'
+fun String.addGreeting(): String {
+    return "Hello, $this"
+}
+
+// Extension function dengan parameter
+fun String.repeatTimes(n: Int): String {
+    return this.repeat(n)
+}
