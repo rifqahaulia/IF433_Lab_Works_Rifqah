671e02665c8059160c644d3853f0a5ff2f5ecbc1
Rifqah Aulia <rifqahauliasalsabilla173@gmail.com>
2026-05-08 14:47:12 +0700

    week11: create nullable receiver extension

--- src/oop_77820_RifqahAulia/week11/StringExtensions.kt
+++ src/oop_77820_RifqahAulia/week11/StringExtensions.kt
@@ -1,5 +1,6 @@
 package oop_77820_RifqahAulia.week11
 
+
 // Extension function dengan receiver 'this'
 fun String.addGreeting(): String {
     return "Hello, $this"
@@ -8,4 +9,9 @@ fun String.addGreeting(): String {
 // Extension function dengan parameter
 fun String.repeatTimes(n: Int): String {
     return this.repeat(n)
+}
+
+// Nullable receiver extension
+fun String?.isNullOrEmptyCustom(): Boolean {
+    return this == null || this.isEmpty()
 }
