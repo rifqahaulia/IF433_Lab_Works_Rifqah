19073acb0f2020adb44069850a2cc4d48b261c8f
Rifqah Aulia <rifqahauliasalsabilla173@gmail.com>
2026-05-08 15:29:46 +0700

    week10: (task) wrap repository data into ApiResponse

--- src/oop_77820_RifqahAulia/week10/CryptoDashboard.kt
+++ src/oop_77820_RifqahAulia/week10/CryptoDashboard.kt
@@ -7,4 +7,9 @@ fun main() {
     coinRepo.add(Coin("BTC", 1.5))
     coinRepo.add(Coin("ETH", 10.0))
     coinRepo.add(Coin("USDT", 2500.0))
+
+    val response = ApiResponse(
+        "200 OK",
+        coinRepo.getAll()
+    )
 }
