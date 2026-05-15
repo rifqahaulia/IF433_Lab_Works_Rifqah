64af1fb96f8871e4778cf44a6cf3197e422b0011
Rifqah Aulia <rifqahauliasalsabilla173@gmail.com>
2026-05-15 14:46:02 +0700

    week12: create basic try-catch-finally

---
+++ src/oop_77820_RifqahAulia/week12/BasicException.kt
@@ -0,0 +1,12 @@
+package oop_77820_RifqahAulia.week12
+
+fun divide(a: Int, b: Int): Int {
+    try {
+        return a / b
+    } catch (e: ArithmeticException) {
+        println("Error: ${e.message}")
+        return -1
+    } finally {
+        println("Division attempt finished")
+    }
+}
