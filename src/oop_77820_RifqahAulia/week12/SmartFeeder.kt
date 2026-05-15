33708f84b4fcc8b77f20a23f83a08410ccacc5b3
Rifqah Aulia <rifqahauliasalsabilla173@gmail.com>
2026-05-15 14:54:20 +0700

    week12: (task) implement food stock validation with throw

--- src/oop_77820_RifqahAulia/week12/SmartFeeder.kt
+++ src/oop_77820_RifqahAulia/week12/SmartFeeder.kt
@@ -14,5 +14,11 @@ fun dispenseKibble(
         throw DispenserJamException()
     }
 
-    return availableGram
+    if (requestedGram > availableGram) {
+        throw FoodEmptyException(requestedGram, availableGram)
+    }
+
+    println("Kibble berhasil dikeluarkan!")
+
+    return availableGram - requestedGram
 }
