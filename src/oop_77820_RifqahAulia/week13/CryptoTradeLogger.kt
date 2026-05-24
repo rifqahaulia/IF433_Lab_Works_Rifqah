55a3cefc03d7a731fc228b8db126f22d47717cf5
Rifqah Aulia <rifqahauliasalsabilla173@gmail.com>
2026-05-24 20:10:20 +0700

    week13: (task) create TradeRecord data class

---
+++ src/oop_77820_RifqahAulia/week13/CryptoTradeLogger.kt
@@ -0,0 +1,12 @@
+package oop_77820_RifqahAulia.week13
+
+import java.io.File
+import java.io.FileNotFoundException
+
+data class TradeRecord(
+    val id: Int,
+    val symbol: String,
+    val type: String,
+    val margin: Double,
+    val pnl: Double
+)
