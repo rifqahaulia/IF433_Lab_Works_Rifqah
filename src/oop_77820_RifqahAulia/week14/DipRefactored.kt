d31868432f32e8a20bfbfb6bf1140a10efde0be3
Rifqah Aulia <rifqahauliasalsabilla173@gmail.com>
2026-06-04 20:15:35 +0700

    week14: create Database abstraction and implementations

---
+++ src/oop_77820_RifqahAulia/week14/DipRefactored.kt
@@ -0,0 +1,9 @@
+package oop_77820_RifqahAulia.week14
+
+interface Database {
+    fun query(sql: String): List<String>
+}
+
+class PostgresDatabase : Database {
+    override fun query(sql: String) = listOf("pg_data1", "pg_data2")
+}
